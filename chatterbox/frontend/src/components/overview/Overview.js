929182ac0976f193603a765a1cecb36535de6059
bernardotavares <bernardopt@goparity.com>
2020-04-05 00:34:14 +0100

    trying to use sb admin 2

--- chatterbox/frontend/src/components/overview/Overview.js
+++ chatterbox/frontend/src/components/overview/Overview.js
@@ -2,10 +2,218 @@ import React, { Fragment } from 'react';
 import Summary from './Summary';
 import Progress from './Progress';
 import StudentsList from './StudentsList';
+import jquery from 'jquery';
+window.$ = window.jQuery = jquery;
+import '../../assets/css/sb-admin-2.css';
+import '../../assets/vendor/fontawesome-free/css/all.min.css';
+require('../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js');
+require('../../assets/vendor/jquery-easing/jquery.easing.min.js');
+require('../../assets/js/sb-admin-2.min.js');
+import '../../assets/vendor/chart.js/Chart.min.js';
+// require('../../assets/js/demo/chart-area-demo.js');
+// import '../../assets/js/demo/chart-pie-demo.js';
+import undraw_posting_photo from '../../assets/chatterboxlogo.png'; 
 
 export default function Overview() {
     return (
         <Fragment>
+            <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+                <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                    <div className="sidebar-brand-icon rotate-n-15">
+                    <i className="fas fa-laugh-wink"></i>
+                    </div>
+                    <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                </a>
+
+                <hr className="sidebar-divider my-0"></hr>
+
+                <li className="nav-item active">
+                    <a className="nav-link" href="index.html">
+                    <i className="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+                </li>
+
+                <li className="nav-item">
+                    <a className="nav-link" href="tables.html">
+                    <i className="fas fa-fw fa-table"></i>
+                    <span>Tables</span></a>
+                </li>
+
+                <hr className="sidebar-divider d-none d-md-block"></hr>
+
+                <div className="text-center d-none d-md-inline">
+                    <button className="rounded-circle border-0" id="sidebarToggle"></button>
+                </div>
+
+            </ul>
+            
+            <div id="content-wrapper" className="d-flex flex-column">
+
+            <div id="content">
+
+                <div className="container-fluid mt-4">
+
+                <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+                </div>
+
+                <div className="row">
+
+                    <div className="col-xl-3 col-md-6 mb-4">
+                    <div className="card border-left-primary shadow h-100 py-2">
+                        <div className="card-body">
+                        <div className="row no-gutters align-items-center">
+                            <div className="col mr-2">
+                            <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
+                            <div className="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            </div>
+                            <div className="col-auto">
+                            <i className="fas fa-calendar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="col-xl-3 col-md-6 mb-4">
+                    <div className="card border-left-success shadow h-100 py-2">
+                        <div className="card-body">
+                        <div className="row no-gutters align-items-center">
+                            <div className="col mr-2">
+                            <div className="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
+                            <div className="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                            </div>
+                            <div className="col-auto">
+                            <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="col-xl-3 col-md-6 mb-4">
+                    <div className="card border-left-info shadow h-100 py-2">
+                        <div className="card-body">
+                        <div className="row no-gutters align-items-center">
+                            <div className="col mr-2">
+                            <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+                            <div className="row no-gutters align-items-center">
+                                <div className="col-auto">
+                                <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                </div>
+                                <div className="col">
+                                <div className="progress progress-sm mr-2">
+                                    <div className="progress-bar bg-info" role="progressbar" style={{width: '50%' }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                            <div className="col-auto">
+                            <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="col-xl-3 col-md-6 mb-4">
+                    <div className="card border-left-warning shadow h-100 py-2">
+                        <div className="card-body">
+                        <div className="row no-gutters align-items-center">
+                            <div className="col mr-2">
+                            <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
+                            <div className="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                            </div>
+                            <div className="col-auto">
+                            <i className="fas fa-comments fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+
+                <div className="row">
+
+                    <div className="col-xl-8 col-lg-7">
+                    <div className="card shadow mb-4">
+                        <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 className="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                        <div className="dropdown no-arrow">
+                            <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                            </a>
+                            <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                            <div className="dropdown-header">Dropdown Header:</div>
+                            <a className="dropdown-item" href="#">Action</a>
+                            <a className="dropdown-item" href="#">Another action</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="card-body">
+                        <div className="chart-area">
+                            <canvas id="myAreaChart"></canvas>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="col-xl-4 col-lg-5">
+                    <div className="card shadow mb-4">
+                        <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 className="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+                        <div className="dropdown no-arrow">
+                            <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                            </a>
+                            <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                            <div className="dropdown-header">Dropdown Header:</div>
+                            <a className="dropdown-item" href="#">Action</a>
+                            <a className="dropdown-item" href="#">Another action</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="card-body">
+                        <div className="chart-pie pt-4 pb-2">
+                            <canvas id="myPieChart"></canvas>
+                            <script src='../../assets/js/demo/chart-pie-demo.js'></script>
+                        </div>
+                        <div className="mt-4 text-center small">
+                            <span className="mr-2">
+                            <i className="fas fa-circle text-primary"></i> Direct
+                            </span>
+                            <span className="mr-2">
+                            <i className="fas fa-circle text-success"></i> Social
+                            </span>
+                            <span className="mr-2">
+                            <i className="fas fa-circle text-info"></i> Referral
+                            </span>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                </div>
+
+            </div>
+
+            <footer className="sticky-footer bg-white">
+                <div className="container my-auto">
+                    <div className="copyright text-center my-auto">
+                        <span>Copyright &copy; Chatterbox 2020</span>
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+        
             <Summary />
             <Progress />
             <StudentsList />
